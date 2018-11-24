1f14c65208d85f18f9deb00f94b7c0e6d148e091
Vienmarskinis <Vienmarskinis@users.noreply.github.com>
2018-11-24 15:54:45 +0100

    added test mode to faulty_trubine

--- src/faulty_turbine/faulty_turbine.py
+++ src/faulty_turbine/faulty_turbine.py
@@ -23,11 +23,45 @@ def fum3(P, offset):
         return 0
 
 
-def fu_data(data, failNum=2, minFaulty=4, maxFaulty=7, minBroken=3, maxBroken=5, fum=0, maxPower=7500000, reduce=0.75, offset=10000):
+def fu_data(data, failNum=2, minFaulty=4, maxFaulty=7, minBroken=3, maxBroken=5, fum=0, maxPower=7500000, reduce=0.75, offset=10000, test=0):
     data0=data.copy()
     n=failNum
     m=0
     s=len(data0)/n
+    
+    if test==1: 
+        faultyStart=100
+        brokenStart=randint(minFaulty*24,maxFaulty*24)+faultyStart
+        brokenEnd=randint(minBroken*24,maxBroken*24)+brokenStart
+        
+        m=m+1
+
+        i=0
+        if (fum==0):
+            method=randint(1,3)
+        else:
+            method=fum
+            
+        print("Starting {} iteration of faults. Fault starts at {};".format(m, faultyStart))
+        print("the engine breaks at {} and is repaired at {}. Fault type was {}".format(brokenStart, brokenEnd,method))
+        
+        for p in data0['output_power']:
+            i=i+1
+            if ((i>faultyStart)&(i<brokenStart)):
+                if method==1:
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum1(maxPower)
+                    data0.iloc[i, data0.columns.get_loc('state')] = 'f'
+                elif method==2:
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum2(p,reduce)
+                    data0.iloc[i, data0.columns.get_loc('state')] = 'f'
+                elif method==3:
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum3(p, offset)
+                    data0.iloc[i, data0.columns.get_loc('state')] = 'f'
+            elif ((i>=brokenStart)&(i<brokenEnd)):
+                data0.iloc[i, data0.columns.get_loc('output_power')] = 0
+                data0.iloc[i, data0.columns.get_loc('state')] = 'r'
+    return data0
+        
         
     while n>0:
         n=n-1
@@ -56,7 +90,7 @@ def fu_data(data, failNum=2, minFaulty=4, maxFaulty=7, minBroken=3, maxBroken=5,
                     data0.iloc[i, data0.columns.get_loc('output_power')] = fum2(p,reduce)
                     data0.iloc[i, data0.columns.get_loc('state')] = 'f'
                 elif method==3:
-                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum4(p, offset)
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum3(p, offset)
                     data0.iloc[i, data0.columns.get_loc('state')] = 'f'
             elif ((i>=brokenStart)&(i<brokenEnd)):
                 data0.iloc[i, data0.columns.get_loc('output_power')] = 0
@@ -72,7 +106,7 @@ def fault_detected (data, dataR, posDet, repairTime=3):
         dataF.iloc[i, dataF.columns.get_loc('state')] = 'r'
     i=0
     for s in dataF['state']:
-        if (i>=posDet+repairTime):
+        if (i>=posDet+repairTime*24):
             if s != 'w':
                 dataF.iloc[i, dataF.columns.get_loc('output_power')] = dataR.iloc[i, dataR.columns.get_loc('output_power')]
                 dataF.iloc[i, dataF.columns.get_loc('state')] = 'w'
