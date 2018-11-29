cfec81a13de124c42365c9bc506804245ee13aa0
Vienmarskinis <Vienmarskinis@users.noreply.github.com>
2018-11-29 16:16:11 +0100

    KPI: efficiency

--- src/communication/wind_turbine/new_wind_turbine.py
+++ src/communication/wind_turbine/new_wind_turbine.py
@@ -1,12 +1,11 @@
 import asyncio
 import threading
 import json
-import sys
-sys.path.insert(0, '../')
-import pandas as pd
 from hbmqtt.client import MQTTClient
 from hbmqtt.mqtt.constants import QOS_1
 from src.faulty_turbine import faulty_turbine
+import pandas as pd
+import sys
 
 
 class WindTurbine(threading.Thread):
@@ -22,38 +21,40 @@ class WindTurbine(threading.Thread):
         self.data['state'] = pd.Series('w', index=data.index)
         self.pointer = 0
         faulty = self.data.copy()
-        self.faulty = faulty_turbine.fu_data(faulty, fum=1, test=1)
+        self.faulty = faulty_turbine.fu_data(faulty, fum=1)
+        self.beforeEff=faulty_turbine.calculate_efficiency(self.faulty)
+        self.afterEff={}
         print('Wind turbine {0} thread ready'.format(id))
 
     def get_data(self):
         data = self.faulty.values[self.pointer, :]
         self.pointer += 1
-        if data[5] == 'w':
-            self.status = 'active'
-        data0 = {'status': self.status,
-                 'time': data[0],
-                 'output_power': data[1],
-                 'wind_speed': data[2],
-                 'temperature': data[3],
-                 'pressure': data[4]}
+        if data[5]=='w':
+            self.status='active'
+        data0={'status': self.status,
+               'time':data[0],
+              'output_power':data[1],
+              'wind_speed':data[2],
+              'temperature':data[3],
+              'pressure':data[4]}
         
         return data0
     
     def process_message(self, msg):
-        if msg == 'stop' and self.status == 'active':
-            real_status = self.faulty['state'].values[self.pointer-1]
-            if real_status == 'w':
+        if (msg == 'stop' and self.status=='active'):
+            realStatus = self.faulty['state'].values[self.pointer-1]
+            if realStatus == 'w':
                 print('Server commited an error, as there was no fault! But I am a good slave, so I stop...')
                 self.status = 'stop'
                 self.faulty = faulty_turbine.server_error(self.faulty, self.data, self.pointer)
                 
-            elif real_status == 'r':
+            elif realStatus == 'r':
                 print("I'm afraid it's too late...")
-                self.status = 'stop'
+                self.status='stop'
             else:
                 print('Server is such a smart guy')
                 self.faulty = faulty_turbine.fault_detected(self.faulty, self.data, self.pointer)
-                self.status = 'stop'
+                self.status='stop'
 
     async def ready(self):
         await self.client.connect(self.broker_uri)
@@ -78,9 +79,11 @@ class WindTurbine(threading.Thread):
     async def system_loop(self):
         await asyncio.sleep(1)
         await self.ready()
-        while self.pointer < self.faulty.shape[0]:
+        while self.pointer<self.faulty.shape[0]:
             await self.publish()
             await self.listen()
+            if self.pointer==self.faulty.shape[0]:
+                self.afterEff=faulty_turbine.calculate_efficiency(self.faulty)
             await asyncio.sleep(0.5)
 
     def run(self):
@@ -91,6 +94,7 @@ if __name__ == '__main__':
 
     assert len(sys.argv) >= 3, "Must supply only 2 arguments.\n" + "Usage: wind_turbine.py id port"
     scriptname, id, port, *arguments = sys.argv
-    df = pd.read_csv('../data/data_simulation.csv')
+    df = pd.read_csv('data/data_simulation.csv')
     wind_turbine = WindTurbine('mqtt://localhost:{0}'.format(port), id, df)
     wind_turbine.start()
+
--- src/faulty_turbine/faulty_turbine.py
+++ src/faulty_turbine/faulty_turbine.py
@@ -1,119 +1,133 @@
 import pandas as pd
 from random import randint
-
 pd.options.display.max_rows = None
 
 
-def fum1(max_power):
-    return randint(0, max_power)
+
+def fum1(maxPower):
+    return randint(0,maxPower)
 
 
-def fum2(power, reduce):
-    return power * reduce
+def fum2(P, reduce):
+    return P*reduce
 
 
-def fum3(power, offset):
-    if power > offset:
-        return power - offset
+def fum3(P, offset):
+    if(P>offset):
+        return P-offset
     else:
         return 0
 
 
-def fu_data(data, failNum=2, minFaulty=4, maxFaulty=7, minBroken=3, maxBroken=5, fum=0, maxPower=7500000, reduce=0.75,
-            offset=10000, test=0):
-    data0 = data.copy()
-    n = failNum
-    m = 0
-    s = len(data0) / n
+def fu_data(data, failNum=2, minFaulty=4, maxFaulty=7, minBroken=3, maxBroken=5, fum=0, maxPower=7500000, reduce=0.75, offset=10000, test=0):
+    data0=data.copy()
+    n=failNum
+    m=0
+    s=len(data0)/n
+    
+    if test==1: 
+        faultyStart=100
+        brokenStart=randint(minFaulty*24,maxFaulty*24)+faultyStart
+        brokenEnd=randint(minBroken*24,maxBroken*24)+brokenStart
+        
+        m=m+1
 
-    if test == 1:
-        faulty_start = 100
-        broken_start = randint(minFaulty * 24, maxFaulty * 24) + faulty_start
-        broken_end = randint(minBroken * 24, maxBroken * 24) + broken_start
-
-        m = m + 1
-
-        i = 0
-        if fum == 0:
-            method = randint(1, 3)
+        i=0
+        if (fum==0):
+            method=randint(1,3)
         else:
-            method = fum
-
-        print("Starting {} iteration of faults. Fault starts at {};".format(m, faulty_start))
-        print("the engine breaks at {} and is repaired at {}. Fault type was {}".format(broken_start, broken_end, method))
-
+            method=fum
+            
+        print("Starting {} iteration of faults. Fault starts at {};".format(m, faultyStart))
+        print("the engine breaks at {} and is repaired at {}. Fault type was {}".format(brokenStart, brokenEnd,method))
+        
         for p in data0['output_power']:
-            i = i + 1
-            if (i > faulty_start) & (i < broken_start):
-                if method == 1:
-                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum1(maxPower)                    
-                elif method == 2:
-                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum2(p, reduce)
-                elif method == 3:
+            i=i+1
+            if ((i>faultyStart)&(i<brokenStart)):
+                if method==1:
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum1(maxPower)
+                elif method==2:
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum2(p,reduce)
+                elif method==3:
                     data0.iloc[i, data0.columns.get_loc('output_power')] = fum3(p, offset)
                 data0.iloc[i, data0.columns.get_loc('state')] = 'f'
-            elif (i >= broken_start) & (i < broken_end):
+            elif ((i>=brokenStart)&(i<brokenEnd)):
                 data0.iloc[i, data0.columns.get_loc('output_power')] = 0
                 data0.iloc[i, data0.columns.get_loc('state')] = 'r'
         return data0
-    while n > 0:
-        n = n - 1
-        faulty_start = randint(300 + m * s, len(data0) - s * n - 300)
-        broken_start = randint(minFaulty * 24, maxFaulty * 24) + faulty_start
-        broken_end = randint(minBroken * 24, maxBroken * 24) + broken_start
+    while n>0:
+        n=n-1
+        faultyStart=randint(300+m*s,len(data0)-s*n-300)
+        brokenStart=randint(minFaulty*24,maxFaulty*24)+faultyStart
+        brokenEnd=randint(minBroken*24,maxBroken*24)+brokenStart
+        
+        m=m+1
 
-        m = m + 1
-
-        i = 0
-        if fum == 0:
-            method = randint(1, 3)
+        i=0
+        if (fum==0):
+            method=randint(1,3)
         else:
-            method = fum
-
-        print("Starting {} iteration of faults. Fault starts at {};".format(m, faulty_start))
-        print("the engine breaks at {} and is repaired at {}. Fault type was {}".format(broken_start, broken_end, method))
-
+            method=fum
+            
+        print("Starting {} iteration of faults. Fault starts at {};".format(m, faultyStart))
+        print("the engine breaks at {} and is repaired at {}. Fault type was {}".format(brokenStart, brokenEnd,method))
+        
         for p in data0['output_power']:
-            i = i + 1
-            if (i > faulty_start) & (i < broken_start):
-                if method == 1:
+            i=i+1
+            if ((i>faultyStart)&(i<brokenStart)):
+                if method==1:
                     data0.iloc[i, data0.columns.get_loc('output_power')] = fum1(maxPower)
                     data0.iloc[i, data0.columns.get_loc('state')] = 'f'
-                elif method == 2:
-                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum2(p, reduce)
+                elif method==2:
+                    data0.iloc[i, data0.columns.get_loc('output_power')] = fum2(p,reduce)
                     data0.iloc[i, data0.columns.get_loc('state')] = 'f'
-                elif method == 3:
+                elif method==3:
                     data0.iloc[i, data0.columns.get_loc('output_power')] = fum3(p, offset)
                     data0.iloc[i, data0.columns.get_loc('state')] = 'f'
-            elif (i >= broken_start) & (i < broken_end):
+            elif ((i>=brokenStart)&(i<brokenEnd)):
                 data0.iloc[i, data0.columns.get_loc('output_power')] = 0
                 data0.iloc[i, data0.columns.get_loc('state')] = 'r'
     return data0
-
-
-def fault_detected(data, data_r, pos_det, repair_time=3):
-    data_f = data.copy()
-    gen = (i for i, x in enumerate(data_f['output_power']) if ((i >= pos_det) & (i < pos_det + 24 * repair_time)))
+    
+    
+def fault_detected (data, dataR, posDet, repairTime=3):
+    dataF=data.copy()
+    gen = (i for i,x in enumerate(dataF['output_power']) if ((i>=posDet)&(i<posDet+24*repairTime)))
     for i in gen:
-        data_f.iloc[i, data_f.columns.get_loc('output_power')] = 0
-        data_f.iloc[i, data_f.columns.get_loc('state')] = 'r'
-    i = 0
-    for s in data_f['state']:
-        if i >= pos_det + repair_time * 24:
+        dataF.iloc[i, dataF.columns.get_loc('output_power')] = 0
+        dataF.iloc[i, dataF.columns.get_loc('state')] = 'r'
+    i=0
+    for s in dataF['state']:
+        if i >= posDet + repairTime * 24:
             if s != 'w':
-                data_f.iloc[i, data_f.columns.get_loc('output_power')] = data_r.iloc[
-                    i, data_r.columns.get_loc('output_power')]
-                data_f.iloc[i, data_f.columns.get_loc('state')] = 'w'
+                dataF.iloc[i, dataF.columns.get_loc('output_power')] = dataR.iloc[i, dataR.columns.get_loc('output_power')]
+                dataF.iloc[i, dataF.columns.get_loc('state')] = 'w'
             else:
-                return data_f
-        i = i + 1
-    return data_f
+                return dataF
+        i=i+1
+    return dataF
 
 
-def server_error(data, dataR, pos_det, repair_time=3):  # TODO check this
-    data_f = data.copy()
-    gen = (i for i, x in enumerate(data_f['output_power']) if ((i >= pos_det) & (i < pos_det + 24 * repair_time)))
+def server_error (data, dataR, posDet, repairTime=3):
+    dataF=data.copy()
+    gen = (i for i,x in enumerate(dataF['output_power']) if ((i>=posDet)&(i<posDet+24*repairTime)))
     for i in gen:
-        data_f.iloc[i, data_f.columns.get_loc('output_power')] = 0
-        data_f.iloc[i, data_f.columns.get_loc('state')] = 'r'
-    return data_f
+        dataF.iloc[i, dataF.columns.get_loc('output_power')] = 0;
+        dataF.iloc[i, dataF.columns.get_loc('state')] = 'r'
+    return dataF
+
+def calculate_efficiency (data):
+    wasWorking=0
+    wasFaulty=0
+    wasBroken=0
+    for p in data['state']:
+        if p == 'w': 
+            wasWorking +=1
+        if p == 'f': 
+            wasFaulty +=1
+        if p == 'r': 
+            wasBroken +=1
+    d={'working':wasWorking,
+       'faulty':wasFaulty,
+       'broken':wasBroken}
+    return d
